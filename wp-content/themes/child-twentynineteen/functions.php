6d0c0686e25a3e0c501e647f829a4a3c301626c6
Mike Agbelou <mike.agbelou@gmail.com>
2021-02-06 22:45:53 +0100

    new commit

--- wp-content/themes/child-twentynineteen/functions.php
+++ wp-content/themes/child-twentynineteen/functions.php
@@ -1,2 +1,6 @@
 <?php
 add_action( "wp_enqueue_scripts", "my_them_enqueue_styles");
+function my_them_enqueue_styles(){
+    wp_enqueue_style("parent-style",get_template_directory_uri()."./styles.css");
+    wp_enqueue_style("child-style",get_stylesheet_directory_uri()."./styles.css");
+}
